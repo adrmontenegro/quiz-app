6389714888afa72172a8a57c36a1cdc34267f7df
Adrian Montenegro <adrmontenegro97@gmail.com>
2023-06-14 17:09:35 -0500

    go back button

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/gradient_container.dart';
 import 'package:quiz_app/question_screen.dart';
 import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/styled_text.dart';
 
 class Quiz extends StatefulWidget {
   @override
@@ -22,18 +23,36 @@ class _QuizState extends State<Quiz> {
 
   void switchScreen() {
     setState(() {
-      activeScreen = "question-screen";
+      if (activeScreen == "start-screen") {
+        activeScreen = "question-screen";
+      } else {
+        activeScreen = "start-screen";
+      }
     });
   }
 
   @override
   Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == "question-screen") {
+      screenWidget = const QuestionScreen();
+    }
+
+    // Here we can modify the [screenWidget] using a ternary operation
+    // var screenWidget = activeScreen == "start-screen"
+    //     ? StartScreen((switchScreen))
+    //     : const QuestionScreen();
+
     return MaterialApp(
       home: Scaffold(
-        body: GradientContainer.home(
-            content: activeScreen == "start-screen"
-                ? StartScreen((switchScreen))
-                : const QuestionScreen()),
+        floatingActionButton: activeScreen == "start-screen"
+            ? null
+            : ElevatedButton.icon(
+                onPressed: switchScreen,
+                icon: const Icon(Icons.arrow_back_rounded),
+                label: const StyledText.button(text: "Back")),
+        body: GradientContainer.home(content: screenWidget),
       ),
     );
   }
